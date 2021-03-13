cb0cf9a28d261b0836331cbdc9da951337990e87
KlaraWK <77977332+KlaraWK@users.noreply.github.com>
2021-03-13 19:11:56 +0100

    Add Form & Seach Function

--- src/app.js
+++ src/app.js
@@ -34,10 +34,23 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
+    let apiKey = "6b34a5900264dc73092b5b2546b2d76b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayTemperature)
+}
 
-let apiKey = "6b34a5900264dc73092b5b2546b2d76b";
-let city = "Sydney"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    //console.log(cityInputElement.value);
+}
+
+search("New York");
 
 //console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature)
+
+
+let form = document.querySelector("#search-from");
+form.addEventListener("submit", handleSubmit);
