dfe34b214f1526fd4b297b7ffd42572995c61781
KlaraWK <77977332+KlaraWK@users.noreply.github.com>
2021-03-14 13:29:14 +0100

    Https & small HTML changes

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` );
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` );
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
     celsiusTemperature = response.data.main.temp;
